87c3c0b1da6b76e4efe78025a6bcd3431fd82952
Shivani Kapadia <shivanikapadia13@gmail.com>
2025-06-30 16:28:05 -0400

    Part 3

--- Server/server.js
+++ Server/server.js
@@ -101,6 +101,117 @@ app.get("/api/planets/:id", async (req, res) => {
     }
 })
 
+app.get("/api/films/:id/characters", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("films_characters");
+        const characters = await collection.find({ film_id: parseInt(id)}).sort({"character_id":1}).toArray()
+        const ids = characters.map(character =>  character.character_id)
+        const charcollection = db.collection("characters");
+        const filmcharacters = await charcollection.find({ id: {"$in": ids} }).sort({"id":1}).toArray()
+        console.log(characters)
+    
+        res.json(filmcharacters);
+
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Error fetching film characters");
+    }
+})
+
+app.get("/api/films/:id/planets", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("films_planets");
+        const planets = await collection.find({ film_id: parseInt(id)}).sort({"planet_id":1}).toArray()
+        const ids = planets.map(planet =>  planet.planet_id)
+        const plancollection = db.collection("planets");
+        const filmplanets = await plancollection.find({ id: {"$in": ids} }).sort({"id":1}).toArray()
+
+
+        res.json(filmplanets);
+
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Error fetching film planets");
+    }
+})
+
+
+app.get("/api/characters/:id/films", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("films_characters");
+        const charfilm = await collection.find({ character_id: parseInt(id)}).sort({"film_id":1}).toArray()
+        const ids = charfilm.map(films =>  films.film_id)
+        const filmcollection = db.collection("films");
+        const characterfilms = await filmcollection.find({ id: {"$in": ids} }).sort({"id":1}).toArray()
+        console.log(charfilm)
+
+
+        res.json(characterfilms);
+
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Error fetching film planets");
+    }
+})
+
+
+app.get("/api/planet/:id/films", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("films_planets");
+        const planetFilm = await collection.find({ planet_id: parseInt(id)}).sort({"film_id":1}).toArray()
+        const ids = planetFilm.map(films =>  films.film_id)
+        const filmcollection = db.collection("films");
+        const planetMovie = await filmcollection.find({ id: {"$in": ids} }).sort({"id":1}).toArray()
+        console.log(planetFilm)
+
+
+        res.json(planetMovie);
+
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Error fetching film planets");
+    }
+})
+
+
+app.get("/api/planet/:id/characters", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("films_planets");
+        const planetFilm = await collection.find({ planet_id: parseInt(id)}).sort({"film_id":1}).toArray()
+        const ids = planetFilm.map(films =>  films.film_id)
+        const filmcollection = db.collection("films");
+        const planetMovie = await filmcollection.find({ id: {"$in": ids} }).sort({"id":1}).toArray()
+        console.log(planetFilm)
+
+
+        res.json(planetMovie);
+
+    } catch (err) {
+        console.error("Error: ", err);
+        res.status(500).send("Error fetching film planets");
+    }
+})
+
 app.listen(3001, () => {
     console.log("Server is running!")
 })
