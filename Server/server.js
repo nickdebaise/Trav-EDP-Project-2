20340f5a299e025a3dc6fda17b20b1d4c999e9b3
wasadmin <wasadmin@example.com>
2025-07-01 11:18:31 -0400

    Final Completion

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
-
+app.use(express.static('./public'))
 app.get("/api/planets", async (req, res) => {
     try {
 
--- react/src/App.jsx
+++ react/src/App.jsx
@@ -9,17 +9,18 @@ import {
 } from "react-router-dom";
 import Film from './components/Film';
 import Planet from './components/Planet';
+import { useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState("")
   return (
     <div>
       <h1>Star Wars Universe Lookup</h1>
-      <label htmlFor="searchString">Who you looking for? <span className="small">(Regular expressions are cool
-        here)</span></label>
-      <input id="searchString" />
+      <label htmlFor="searchString">Who you looking for?</label>
+      <input id="searchString" value={search} onChange={e => setSearch(e.target.value)} />
       <Router>
         <Routes>
-          <Route exact path="/" element={(<CharacterList search={""} />)} />
+          <Route exact path="/" element={(<CharacterList search={search} />)} />
           <Route exact path="/characters/:id" element={(<Character />)} />
           <Route exact path="/films/:id" element={(<Film />)} />
           <Route exact path="/planets/:id" element={(<Planet />)} />
